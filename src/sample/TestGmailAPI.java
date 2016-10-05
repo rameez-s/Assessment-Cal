5e56933f3b295fb10d329359a386fb74a2ea24f6
18iwahlqvist <18iwahlqvist@asd.edu.qa>
2016-10-05 15:10:27 +0300

    new file:   .idea/libraries/javax_servlet_3_0.xml 	new file:   .idea/libraries/org_mortbay_jetty.xml 	new file:   .idea/libraries/org_mortbay_jetty_util.xml 	modified:   .idea/workspace.xml 	modified:   JavaFxApplication.iml 	new file:   libs/google-api-client-java6-1.21.0.jar 	new file:   libs/google-oauth-client-java6-1.21.0.jar 	new file:   libs/google-oauth-client-jetty-1.21.0.jar 	new file:   libs/javax.servlet-3.0.jar 	new file:   libs/org.mortbay.jetty.jar 	new file:   libs/org.mortbay.jetty.util.jar 	modified:   out/production/JavaFxApplication/sample/TestGmailAPI.class 	modified:   src/sample/TestGmailAPI.java

--- src/sample/TestGmailAPI.java
+++ src/sample/TestGmailAPI.java
@@ -1,29 +1,101 @@
 package sample;
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.GmailScopes;
+import com.google.api.services.gmail.model.Label;
+import com.google.api.services.gmail.model.ListLabelsResponse;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.security.SecureRandom;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
-
+import java.util.Random;
 
 public class TestGmailAPI{
-    
-    private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
-    private static final JsonFactory JSON_FACTORY = new JacksonFactory();
+    private static final String CALLBACK_URI = "";
+    static GoogleAuthorizationCodeFlow gFlow;
+    private static String client_secret = "jXy0O9yNMaIskpIpqS9STwuw";
+    private static String client_ID = "733309529446-6ic410d707p3ocl3gh2v06uqe83h1psi.apps.googleusercontent.com";
+    private static String stateToken;
+    /** Application name. */
+    private static final String APPLICATION_NAME =
+            "Gmail API Java Quickstart";
+
+    /** Directory to store user credentials for this application. */
+
+    /** Global instance of the {@link FileDataStoreFactory}. */
+
+    /** Global instance of the JSON factory. */
+    private static final JsonFactory JSON_FACTORY =
+            JacksonFactory.getDefaultInstance();
+
+    /** Global instance of the HTTP transport. */
+    private static HttpTransport HTTP_TRANSPORT;
+
+    /** Global instance of the scopes required by this quickstart.
+     *
+     * If modifying these scopes, delete your previously saved credentials
+     * at ~/.credentials/gmail-java-quickstart
+     */
+    private static final List<String> SCOPES =
+            Arrays.asList(GmailScopes.GMAIL_LABELS);
+
+    static {
+        try {
+            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Creates an authorized Credential object.
+     * @return an authorized Credential object.
+     * @throws IOException
+     */
+    public static Credential authorize() throws IOException {
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow =
+                new GoogleAuthorizationCodeFlow.Builder(
+                        HTTP_TRANSPORT, JSON_FACTORY, client_ID, client_secret, SCOPES)
+                        .setAccessType("offline")
+                        .build();
+        Credential credential = new AuthorizationCodeInstalledApp(
+                flow, new LocalServerReceiver()).authorize("user");
+        return credential;
+    }
+
+    /**
+     * Build and return an authorized Gmail client service.
+     * @return an authorized Gmail client service
+     * @throws IOException
+     */
+    public static Gmail getGmailService() throws IOException {
+        Credential credential = authorize();
+        return new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
 
     public static void main(String[] args) throws IOException {
-        GoogleCredential credential = new GoogleCredential();
-        credential.refreshToken();
-        Gmail plus = new Gmail.Builder(new NetHttpTransport(), JacksonFactory.getDefaultInstance(), credential)
-                .setApplicationName("Google-PlusSample/1.0")
-                .build();
+        // Build a new authorized API client service.
+        Gmail service = getGmailService();
+
+        // Print the labels in the user's account.
+        String user = "me";
+        System.out.println(service.users().toString());
     }
 }
