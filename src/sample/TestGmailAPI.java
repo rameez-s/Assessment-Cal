ef09faa555dad20e0d186344ce602331ff61124a
18iwahlqvist <18iwahlqvist@asd.edu.qa>
2016-10-06 09:12:09 +0300

    Completed gmail login 	modified:   .idea/workspace.xml 	modified:   out/production/JavaFxApplication/sample/TestGmailAPI.class 	modified:   src/sample/TestGmailAPI.java

--- src/sample/TestGmailAPI.java
+++ src/sample/TestGmailAPI.java
@@ -6,50 +6,29 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.GmailScopes;
-import com.google.api.services.gmail.model.Label;
-import com.google.api.services.gmail.model.ListLabelsResponse;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.security.SecureRandom;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
-import java.util.Random;
 
 public class TestGmailAPI{
-    private static final String CALLBACK_URI = "";
-    static GoogleAuthorizationCodeFlow gFlow;
     private static String client_secret = "jXy0O9yNMaIskpIpqS9STwuw";
     private static String client_ID = "733309529446-6ic410d707p3ocl3gh2v06uqe83h1psi.apps.googleusercontent.com";
-    private static String stateToken;
-    /** Application name. */
-    private static final String APPLICATION_NAME =
-            "Gmail API Java Quickstart";
 
-    /** Directory to store user credentials for this application. */
+    private static final String NAME_OF_APPLICATION =
+            "Assessment Calendar";
 
-    /** Global instance of the {@link FileDataStoreFactory}. */
-
-    /** Global instance of the JSON factory. */
     private static final JsonFactory JSON_FACTORY =
             JacksonFactory.getDefaultInstance();
 
-    /** Global instance of the HTTP transport. */
     private static HttpTransport HTTP_TRANSPORT;
 
-    /** Global instance of the scopes required by this quickstart.
-     *
-     * If modifying these scopes, delete your previously saved credentials
-     * at ~/.credentials/gmail-java-quickstart
-     */
     private static final List<String> SCOPES =
-            Arrays.asList(GmailScopes.GMAIL_LABELS);
+            Arrays.asList(GmailScopes.GMAIL_READONLY);
 
     static {
         try {
@@ -60,14 +39,8 @@ public class TestGmailAPI{
         }
     }
 
-    /**
-     * Creates an authorized Credential object.
-     * @return an authorized Credential object.
-     * @throws IOException
-     */
     public static Credential authorize() throws IOException {
-
-        // Build flow and trigger user authorization request.
+        //authorize the user
         GoogleAuthorizationCodeFlow flow =
                 new GoogleAuthorizationCodeFlow.Builder(
                         HTTP_TRANSPORT, JSON_FACTORY, client_ID, client_secret, SCOPES)
@@ -78,24 +51,20 @@ public class TestGmailAPI{
         return credential;
     }
 
-    /**
-     * Build and return an authorized Gmail client service.
-     * @return an authorized Gmail client service
-     * @throws IOException
-     */
     public static Gmail getGmailService() throws IOException {
         Credential credential = authorize();
+        //return a Gmailservice
         return new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
-                .setApplicationName(APPLICATION_NAME)
+                .setApplicationName(NAME_OF_APPLICATION)
                 .build();
     }
 
     public static void main(String[] args) throws IOException {
-        // Build a new authorized API client service.
         Gmail service = getGmailService();
 
-        // Print the labels in the user's account.
+        //Print the email of the user
         String user = "me";
-        System.out.println(service.users().toString());
+        System.out.println(service.users().getProfile(user).execute().getEmailAddress());
+
     }
 }
